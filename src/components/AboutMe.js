cb16afdf25fee7d883f1b12e035e9dba2080f939
Oscar Estrada <arturostrada2020@gmail.com>
2022-06-08 18:14:43 -0500

    about me content added

---
+++ src/components/AboutMe.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export const AboutMe = () => {
+  return (
+    <div className="w-full min-h-screen px-5 md:px-44 md:pt-20 space-y-10">
+      <p className="text-xl md:text-[18px] leading-relaxed">
+        <span className="text-green-custom">></span>¿Qué Pachuca por Tuluca
+        stalker? ah noo! este no es mi perfil de instagram 😅. Qué onda mundo mi
+        nombre es Oscar tengo 27 años y soy ingeniero en electrónica egresado de
+        la BUAP. Durante los primeros 3 años de mi vida laboral trabajé en la
+        industría automotríz para empresas tier 2 de Audi, Volkswagen y BMW, sin
+        embargo por muchos factores pero en especial la falta de pasión tomé la
+        decisión de hacer un cambio rotundo en mi carrera profesional y fue así
+        como decidí incursionar en la industría del software
+      </p>
+      <p className="text-xl md:text-[18px] leading-relaxed">
+        <span className="text-green-custom">></span>
+        Mi historía como desarrollador web no empieza a mis 12 años hackeando
+        algun sitio web o creando videojuegos, como suele iniciar todas las
+        historias de los genios cibernautas. Si bien es cierto que mi atracción
+        e interés por la programación inicio a muy temprana edad, fue hasta el
+        inicio de mis 20´s cuando tuve mi primer acercamiento real al desarrollo
+        web, cuando la intranet de una de las empresas para las que trabajé
+        sufrió un daño debido a una descarga eléctrica y toda la información
+        almacenada en el servidor se daño. No teniamos departamiento de IT y al
+        parecer nadie sabia de computadoras y aunque yo tampoco era un crack
+        programando, en mis tiempos libres siempre me gustaba escudriñar sobre
+        tecnología y esas cosas freakies
+      </p>
+      <p className="text-xl md:text-[18px] leading-relaxed">
+        <span className="text-green-custom">></span>
+        Para no hacerte el cuento largo, me eche el muertito y asumí la
+        responsabilidad. Extraje la información de los discos duros, hice un
+        formateo del sistema operativo, monte nuevamente la interfaz, conecte la
+        base de datos e hice un backup de respaldo por si llegaba a suceder de
+        nuevo algo inesperado. Ya se que quizá suene muy pro pero la realidad es
+        que no sabia absolutamente nada de lo que estaba haciendo, simplemente
+        me la creí y tome acción, claro sin olvidar que el Dios Google y Youtube
+        siempre me ayudarón
+      </p>
+      <p className="text-xl md:text-[18px] leading-relaxed">
+        <span className="text-green-custom">></span>
+        Más allá de como lo logré, hubo algo que cambio rotundamente en mí,
+        sentía por primera vez que mi trabajo realmente era útil. Si bien es
+        cierto que en la carrera de electrónica se nos enseña a programar en
+        lenguajes de bajo nivel como ensamblador, C o incluso lenguajes de
+        descripción de hardware como VHDL, siempre fue muy abstracta su
+        aplicación, nunca habia experimentado de manera tangible una aplicación
+        real_
+      </p>
+    </div>
+  );
+};
