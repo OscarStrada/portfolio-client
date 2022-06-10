43c847e493c2673a92bf1f64d590b0ab85234d86
Oscar Estrada <arturostrada2020@gmail.com>
2022-06-09 19:09:21 -0500

    skill section added

---
+++ src/components/Skills.js
@@ -0,0 +1,155 @@
+import React from 'react';
+
+export const Skills = () => {
+  return (
+    <div className="w-full min-h-screen px-5 md:px-20 space-y-10">
+      <h2 className="text-lg">oestrada@Habilidades ~ % ls -la</h2>
+      <div className="w-full flex md:grid md:grid-cols-9">
+        <div className="hidden md:block">
+          <p className="text-xl leading-relaxed">drwx----</p>
+          <p className="text-xl leading-relaxed">-rw---r-</p>
+          <p className="text-xl leading-relaxed">-rw---r-</p>
+          <p className="text-xl leading-relaxed">drwx----</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">drwx----</p>
+          <p className="text-xl leading-relaxed">-rw---r-</p>
+          <p className="text-xl leading-relaxed">-rw---r-</p>
+          <p className="text-xl leading-relaxed">drwx----</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">-rw-x-r-</p>
+          <p className="text-xl leading-relaxed">drwx----</p>
+          <p className="text-xl leading-relaxed">drwx----</p>
+        </div>
+        <div>
+          <p className="md:text-xl leading-relaxed">HTML</p>
+          <p className="md:text-xl leading-relaxed">CSS</p>
+          <p className="md:text-xl leading-relaxed">SASS</p>
+          <p className="md:text-xl leading-relaxed">JavaScript</p>
+          <p className="md:text-xl leading-relaxed">Tailwind</p>
+          <p className="md:text-xl leading-relaxed">git</p>
+          <p className="md:text-xl leading-relaxed">Github</p>
+          <p className="md:text-xl leading-relaxed">React</p>
+          <p className="md:text-xl leading-relaxed">Netlify</p>
+          <p className="md:text-xl leading-relaxed">Vercel</p>
+          <p className="md:text-xl leading-relaxed">Sanity</p>
+          <p className="md:text-xl leading-relaxed">GSAP</p>
+          <p className="md:text-xl leading-relaxed">Nodejs</p>
+          <p className="md:text-xl leading-relaxed">Express</p>
+          <p className="md:text-xl leading-relaxed">Nextjs</p>
+          <p className="md:text-xl leading-relaxed">Azure</p>
+        </div>
+        <div className="col-start-3 col-end-5 text-center">
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Avanzado</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Intermedio</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Intermedio</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Intermedio</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Intermedio</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Intermedio</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Básico</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Básico</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Básico</p>
+          <p className="col-span-3 md:text-xl leading-relaxed">Básico</p>
+        </div>
+        <div className="hidden md:block">
+          <p className="md:text-xl leading-relaxed">1088</p>
+          <p className="md:text-xl leading-relaxed">130</p>
+          <p className="md:text-xl leading-relaxed">7</p>
+          <p className="md:text-xl leading-relaxed">3370</p>
+          <p className="md:text-xl leading-relaxed">132</p>
+          <p className="md:text-xl leading-relaxed">192</p>
+          <p className="md:text-xl leading-relaxed">341</p>
+          <p className="md:text-xl leading-relaxed">1</p>
+          <p className="md:text-xl leading-relaxed">2051</p>
+          <p className="md:text-xl leading-relaxed">3794</p>
+          <p className="md:text-xl leading-relaxed">98</p>
+          <p className="md:text-xl leading-relaxed">33</p>
+          <p className="md:text-xl leading-relaxed">230</p>
+          <p className="md:text-xl leading-relaxed">539</p>
+          <p className="md:text-xl leading-relaxed">88</p>
+          <p className="md:text-xl leading-relaxed">1</p>
+        </div>
+        <div className="hidden md:block">
+          <p className="md:text-xl leading-relaxed">Jan</p>
+          <p className="md:text-xl leading-relaxed">Feb</p>
+          <p className="md:text-xl leading-relaxed">May</p>
+          <p className="md:text-xl leading-relaxed">Jun</p>
+          <p className="md:text-xl leading-relaxed">Jun</p>
+          <p className="md:text-xl leading-relaxed">Sep</p>
+          <p className="md:text-xl leading-relaxed">Oct</p>
+          <p className="md:text-xl leading-relaxed">Oct</p>
+          <p className="md:text-xl leading-relaxed">Oct</p>
+          <p className="md:text-xl leading-relaxed">Oct</p>
+          <p className="md:text-xl leading-relaxed">Nov</p>
+          <p className="md:text-xl leading-relaxed">Nov</p>
+          <p className="md:text-xl leading-relaxed">Dec</p>
+          <p className="md:text-xl leading-relaxed">Dec</p>
+          <p className="md:text-xl leading-relaxed">Dec</p>
+          <p className="md:text-xl leading-relaxed">Dec</p>
+        </div>
+        <div>
+          <p className="md:text-xl leading-relaxed">25</p>
+          <p className="md:text-xl leading-relaxed">2</p>
+          <p className="md:text-xl leading-relaxed">19</p>
+          <p className="md:text-xl leading-relaxed">13</p>
+          <p className="md:text-xl leading-relaxed">27</p>
+          <p className="md:text-xl leading-relaxed">4</p>
+          <p className="md:text-xl leading-relaxed">56</p>
+          <p className="md:text-xl leading-relaxed">81</p>
+          <p className="md:text-xl leading-relaxed">6</p>
+          <p className="md:text-xl leading-relaxed">41</p>
+          <p className="md:text-xl leading-relaxed">74</p>
+          <p className="md:text-xl leading-relaxed">5</p>
+          <p className="md:text-xl leading-relaxed">2</p>
+          <p className="md:text-xl leading-relaxed">61</p>
+          <p className="md:text-xl leading-relaxed">120</p>
+          <p className="md:text-xl leading-relaxed">6</p>
+        </div>
+        <div>
+          <p className="md:text-xl leading-relaxed">15:17</p>
+          <p className="md:text-xl leading-relaxed">12:41</p>
+          <p className="md:text-xl leading-relaxed">05:11</p>
+          <p className="md:text-xl leading-relaxed">11:54</p>
+          <p className="md:text-xl leading-relaxed">16:32</p>
+          <p className="md:text-xl leading-relaxed">08:18</p>
+          <p className="md:text-xl leading-relaxed">15:57</p>
+          <p className="md:text-xl leading-relaxed">20:01</p>
+          <p className="md:text-xl leading-relaxed">21:08</p>
+          <p className="md:text-xl leading-relaxed">02:12</p>
+          <p className="md:text-xl leading-relaxed">19:14</p>
+          <p className="md:text-xl leading-relaxed">13:16</p>
+          <p className="md:text-xl leading-relaxed">17:17</p>
+          <p className="md:text-xl leading-relaxed">04:05</p>
+          <p className="md:text-xl leading-relaxed">08:18</p>
+          <p className="md:text-xl leading-relaxed">23:07</p>
+        </div>
+        <div>
+          <p className="md:text-xl leading-relaxed">..</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">.npm</p>
+          <p className="md:text-xl leading-relaxed">.git</p>
+          <p className="md:text-xl leading-relaxed">.ssh</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">..</p>
+          <p className="md:text-xl leading-relaxed">..</p>
+          <p className="md:text-xl leading-relaxed">.npm</p>
+          <p className="md:text-xl leading-relaxed">.ssh</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">.</p>
+          <p className="md:text-xl leading-relaxed">.ssh</p>
+          <p className="md:text-xl leading-relaxed">.ssh</p>
+        </div>
+      </div>
+    </div>
+  );
+};
